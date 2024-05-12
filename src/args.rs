8b582ece30ff27b491e4cce74ebde704847ab4d0
Denis <mehillideny13@gmail.com>
2024-05-12 21:54:21 +0200

    removed examples

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,14 @@
-use clap::{builder::Str, Args, Parser, Subcommand};
+use clap::{
+    builder::Str, Args, Parser, Subcommand
+};
+
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct SpigotflyArgs {
+    
     #[clap(subcommand)]
-    pub command: SpigotflyCommand,
+    pub command : SpigotflyCommand,
 }
 
 #[derive(Debug, Subcommand)]
@@ -13,21 +17,25 @@ pub enum SpigotflyCommand {
     Search(SearchCommand),
 
     /// Download a resource by id
-    Download(DownloadCommand),
+    Download(DownloadCommand)
+
 }
+
 
 #[derive(Debug, Args)]
 pub struct SearchCommand {
+
     /// Search by tag
     #[clap(long, short, action)]
     pub tag: bool,
 
     /// Text to be searched
-    pub query: Vec<String>,
+    pub query : Vec<String>
 }
 
 #[derive(Debug, Args)]
 pub struct DownloadCommand {
+
     /// Text to be searched
-    pub id: u32,
+    pub id : u32
 }
